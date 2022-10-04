a85b88cce9505bd1feae129b0f3e1d08412b230f
Kame2021 <21cm0108@jec.ac.jp>
2022-10-04 21:33:14 +0900

    Initial Commit

---
+++ DatePickerSample/DatePickerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerSampleApp.swift
+//  DatePickerSample
+//
+//  Created by Ryo on 2022/10/04.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
